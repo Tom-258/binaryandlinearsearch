e58630136ab2539f2fd7938c8f5996be720dfe55
Tom-258 <thomasstanger0@gmail.com>
2021-05-11 19:21:53 +0100

    updated code

--- Linear and Binary Search/Program.cs
+++ Linear and Binary Search/Program.cs
@@ -6,26 +6,53 @@ namespace Linear_and_Binary_Search
     {
         static void Main(string[] args)
         {
-            var arr = new int[]{0,1,2,3,4,5,6,10,18, 27, 63, 72, 96,97,98};
+            var arr = new int[] {0, 1, 2, 3, 4, 5, 6, 10, 18, 27, 63, 72, 96, 97, 98};
             var n = int.Parse(Console.ReadLine());
-            LinearSearch(arr,n);
-        }
-        
-        static void LinearSearch(int[] Arr, int n)
-        {
-            var i = 0;
-            do
+            LinearSearch(arr, n);
+            BinarySearch(arr, n);
+
+            static void LinearSearch(int[] arr, int n)
             {
-                if (Arr[i] == n )
+                var i = 0;
+                do
                 {
-                    Console.WriteLine("The number is at {0}", i);
-                    break;
+                    if (arr[i] == n)
+                    {
+                        Console.WriteLine("The number is at {0}", i);
+                        break;
+                    }
+
+                    i += 1;
+
+                } while (i < arr.Length);
+            }
+
+            static void BinarySearch(int[] arr, int n)
+            {
+                var isInlist = false;
+                var i = arr.Length / 2 - 1;
+                
+                while (!isInlist)
+                {
+                    if (n < arr[i])
+                        i -= i / 2;
+                    if (n > arr[i])
+                        i += i / 2;
+                    if (n == arr[i])
+                    {
+                        isInlist = true;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
 
-                i += 1;
-
-            } while (i < Arr.Length);
+                if (isInlist)
+                    Console.WriteLine("Number is at {0}", i);
+                else
+                    Console.WriteLine("Number is not in list");
+            }
         }
-        
     }
 }
